8693b2e90e5c149b351bae780925fb2d2a04b5c6
Salman Farshi <sfarshi222@gmail.com>
2026-09-09 22:16:20 +0600

    add side heading component

---
+++ src/Components/SideHeading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SideHeading({tittle}) {
+  return (
+    <h3 className='text-[18px] md:text-[24px] lg:text-[28px] text-secondary font-bold font-quicksand'>{tittle}</h3>
+  )
+}
+
+export default SideHeading
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SideHeading from "../Components/SideHeading";
 
 const shopping = ["/shoping (1).png", "/shoping (2).png", "/shoping (3).png"];
 function Home() {
@@ -22,13 +23,31 @@ function Home() {
       <section className="max-w-360 mx-auto px-4 md:px-6 lg:px-0 mt-4 md:mt-7 lg:mt-12">
         <div className="flex items-center gap-3">
           {shopping.map((items) => (
-            <img src={items} alt="" className="w-[31%] md:w-[33%]" />
+            <img
+              src={items}
+              alt=""
+              className="w-[31%] md:w-[33%] lg:h-70 object-cover"
+            />
           ))}
         </div>
-      </section>
-      {/* ============= 
+
+        {/* ============= 
      shoping section end
      ================= */}
+        {/* ============= 
+     Popular Products section start
+     ================= */}
+
+        <div className=" mt-5 md:mt-10 lg:mt-14">
+          <SideHeading tittle="Popular Products" />
+
+          <div className="mt-3 md:mt-8 lg:mt-11"></div>
+        </div>
+
+        {/* ============= 
+     Popular Products section end
+     ================= */}
+      </section>
     </>
   );
 }
